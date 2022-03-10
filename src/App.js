0c104a7306e45a199139bb1d9d12df4f3eb6627e
Breno Pimenta <brenocsp@gmail.com>
2022-03-10 17:02:00 -0300

    added the sidebar sketch and table for games

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Login from './pages/login/login.js';
 import Cadastro from './pages/cadastro/cadastro.js';
+import MeusJogos from './pages/meus-jogos/meus-jogos.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path='/login' component={Login}/>
           <Route path='/cadastro' component={Cadastro}/>
+          <Route path='/meus-jogos' component={MeusJogos}/>
         </Switch>
     </Router>
   );
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SideBar.css';
+import {Nav} from "react-bootstrap";
+import { Link } from 'react-router-dom'
+
+
+function SideBar(){
+    return (
+        <div className='nav-bar'>
+            <Link to='#'>
+            </Link>
+
+        </div>
+        );
+}
+
+export default SideBar;
